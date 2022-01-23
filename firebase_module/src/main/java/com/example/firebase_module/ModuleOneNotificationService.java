ee091b300317c85730f74e215e1cd57f512ba60b
Mohammad Mirdar <m.mirdar@kianiranian.com>
2022-01-23 14:52:53 +0330

    some firebase dependency added

--- firebase_module/src/main/java/com/example/firebase_module/ModuleOneNotificationService.java
+++ firebase_module/src/main/java/com/example/firebase_module/ModuleOneNotificationService.java
@@ -1,10 +1,12 @@
 package com.example.firebase_module;
 
+import android.content.Context;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.installations.FirebaseInstallations;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -14,8 +16,10 @@ public class ModuleOneNotificationService extends FirebaseMessagingService {
     private static final String TAG = "ModuleOneNotificationSe";
 
 
-    public ModuleOneNotificationService() {
+    public ModuleOneNotificationService(Context context) {
 //        Log.e(TAG, "Token:  " + FirebaseInstanceId.getInstance().getToken());
+
+        FirebaseApp.initializeApp(context);
         FirebaseInstallations.getInstance().getId().addOnSuccessListener(new OnSuccessListener<String>() {
             @Override
             public void onSuccess(String s) {
